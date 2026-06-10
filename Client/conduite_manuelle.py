9d254210ec8316cbf031e7cfd7d005cd8383417b
uvk9ifqsqax3a67 <aurelien.maury@epitech.eu>
2026-06-10 20:43:59 +0200

    Fix commande1

--- Client/conduite_manuelle.py
+++ Client/conduite_manuelle.py
@@ -6,6 +6,14 @@ ROOT = Path(__file__).resolve().parent.parent
 sys.path.append(str(ROOT / "Gamepad"))
 
 import Gamepad
+from pyvesc import encode
+from pyvesc.messages.setters import SetDutyCycle
+import serial
+
+
+VESC_PORT = "/dev/ttyACM0"
+VESC_BAUDRATE = 115200
+MAX_SPEED = 0.20
 
 
 def limiter(value, min_value=-1.0, max_value=1.0):
@@ -18,27 +26,18 @@ def normalize_trigger(value):
     return value
 
 
-def set_motor(throttle):
-    """
-    throttle :
-    -1.0 = marche arrière
-     0.0 = stop
-     1.0 = avant max
+vesc = serial.Serial(VESC_PORT, VESC_BAUDRATE, timeout=0.1)
 
-    À adapter selon ton driver moteur / ESC.
-    """
+
+def set_motor(throttle):
+    throttle = limiter(throttle, -MAX_SPEED, MAX_SPEED)
+    packet = encode(SetDutyCycle(throttle))
+    vesc.write(packet)
     print(f"MOTEUR: {throttle:.2f}")
 
 
 def set_steering(steering):
-    """
-    steering :
-    -1.0 = gauche
-     0.0 = centre
-     1.0 = droite
-
-    À adapter selon ton servo de direction.
-    """
+    steering = limiter(steering)
     print(f"DIRECTION: {steering:.2f}")
 
 
@@ -65,7 +64,7 @@ def main():
             rt = normalize_trigger(gamepad.axis("R2"))
             lt = normalize_trigger(gamepad.axis("L2"))
 
-            throttle = limiter(rt - lt) * 0.2
+            throttle = limiter(rt - lt) * MAX_SPEED
 
             set_steering(steering)
             set_motor(throttle)
@@ -79,6 +78,7 @@ def main():
         print("Arrêt sécurité")
         stop_car()
         gamepad.disconnect()
+        vesc.close()
 
 
 if __name__ == "__main__":
